b5bd0936c25071b2cfa22fc7b75cef06a876a3cc
Евгений Зотов <zotov_evgeniy@mail.ru>
2025-10-16 04:17:08 +0300

    Добавил проверку запуска

--- 83t460-client.cmd
+++ 83t460-client.cmd
@@ -2,6 +2,7 @@
 @echo off
 REM start asodi client
 
+title 83t460-client
 set prgdir=%~dp0
 set fname=%~n0
 set prgname="%prgdir%%fname%.exe"
@@ -44,7 +45,7 @@ chcp 866 >nul 2>&1
     if /i "%1" == "-c" set CONF=%2 & shift
     if /i "%1" == "-l" set LOG=%2 & shift
     if /i "%1" == "-w" set WEBPort=%2 & shift
-    if /i "%1" == "-s" SERVER=%2 & shift
+    if /i "%1" == "-s" set SERVER=%2 & shift
     if /i "%1" == "-m" set MAC=%2 & shift
     if /i "%1" == "-h" goto usage
     shift
@@ -52,17 +53,41 @@ chcp 866 >nul 2>&1
 
 :: Определяем наличие необхоимых файлов и переменных
 if not CONF == "" if exist %DATADIR%\%CONF% set CONF=%DATADIR%\%CONF%
-rem if exist %CONF% for /f "tokens=1,2 delims=: " %%x in ('powershell -Command "[System.Reflection.Assembly]::LoadWithPartialName('System.Web.Extensions'); $json=Get-Content C:\Users\zotov_qc\source\repos\NewRepo\config.json; $ser = New-Object System.Web.Script.Serialization.JavaScriptSerializer; $ser.DeserializeObject($json)"') do echo %%x=%%y
-if exist %CONF% for /f "tokens=1,2 delims=: " %%x in ('type "%CONF%"') do set %%~x=%%~y
-if not %server_addr:~,-2% == "" set SERVER=%server_addr:~,-2%
+set n=0
+if exist %CONF% for /f "tokens=1,2 delims=:- " %%x in ('2^>nul powershell -Command "[System.Reflection.Assembly]::LoadWithPartialName('System.Web.Extensions'); $json=Get-Content %CONF%; $ser = New-Object System.Web.Script.Serialization.JavaScriptSerializer; $ser.DeserializeObject($json)"') do (
+    set %%x[!n!]=%%~y
+    if "%%x" == "Value" set /a n=n+1
+)
+if exist %CONF% for /f "tokens=1,2 delims=:, " %%x in ('type "%CONF%"') do set %%~x=%%~y
+if not %server_addr% == "" set SERVER=%server_addr%
 if not %server_port% == "" set PORT=%server_port%
-if not %mac:~,-2% == ""  set WEBPort=%mac:~,-2%
+if not %mac% == ""  set WEBPort=%mac%
+FOR /L %%H IN (1,1,%n%) DO ( 
+    if "!Key[%%H]!" == "server_addr" if not "!Value[%%H]!" == "" set SERVER=!Value[%%H]!
+    if "!Key[%%H]!" == "server_port" if not "!Value[%%H]!" == "" set PORT=!Value[%%H]!
+    if "!Key[%%H]!" == "mac" if not "!Value[%%H]!" == "" set WEBPort=!Value[%%H]!
+)
 if %LOG% == "" set LOG=%DATADIR%\83tclient.%lev%.log
 if not %LOG% == "" for /f %%l in ("%LOG%") do if %%~dpl == "." set LOG=%DATADIR%\%LOG%
 
 pushd %prgdir%
+for %%l in (%LOG%) do set dirlog=%%~dpl
+call :chmac -s 0 %dirlog%
+mkdir %dirlog%
+call :chmac -s 0 %dirlog%
 sysapicmd.exe -s 0 %LOG% >nul 2>&1
 call :chmac -s 0 %DATADIR%
+
+:: Проверяем запущенную программу
+for /f "tokens=1,7,2 delims=," %%n IN ('tasklist /nh /v /fo csv /fi "IMAGENAME eq %fname%.exe"') do (
+    set puser=%%~p
+    echo %%n , %%o , %%p
+    echo !puser! , %COMPUTERNAME%, %USERNAME% , %fname%.exe
+    if "!puser:%COMPUTERNAME%\=!" == "%USERNAME%" if "%fname%.exe" == "%%~n" (
+        echo "%prgname% is running in pid: %%~p";
+        exit /b 1
+    )
+)
 for /f %%c in ('dir /b "%CONF%"') do for /f "delims=" %%f in ('2^>nul dir /s /b %DATADIR% ^| findstr /v /c:"%%~nxc"') do >>%LOG% 2>&1 rd /s /q "%%f" >>%LOG% 2>&1 & >>%LOG% 2>&1 del /q "%%f"
 echo. >> %LOG%
 echo. %DATE% %TIME% start %prgname% %SERVER% %PORT% %WEBPort% >> %LOG%
